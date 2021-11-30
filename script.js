57e469f07ac03469657cd41b70c1def1e6058f76
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2021-11-30 23:21:55 +0300

    Part 1

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
-  $('.your-class').slick({
-  });
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 300,
+    easing: 'linear',
+    infinite: true,
+    initialSlide: 0,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true,
+    touchThreshold: 5,
+    touchMove: true,
+    waitForAnimate: true,
+    centerMode: false,
+    variableWidth: true,
+    rows: 1,
+    slidesPerRow: 1,
+    vertical: false,
+    verticalSwiping: false,
+  })
 });
